45b214ac7f3429f30755547c724550fef3b4fcaf
react#1507 <127757116+reactor-coding@users.noreply.github.com>
2023-08-19 16:49:38 +0900

    修正

--- InterCLASS/remove_loading-screen.js
+++ InterCLASS/remove_loading-screen.js
@@ -1,3 +1,12 @@
+const excludeRewriteContextmenu = [
+  "discord.com",
+  "ptb.discord.com",
+  "canary.discord.com",
+  "vscode.dev",
+  "insider.vscode.dev",
+  "replit.com"
+]
+
 /**
  * @file contentScripts/content.js:L4-9, L108-128
  */
@@ -10,5 +19,14 @@ document.querySelector("[id^=\"jbddgjglgkkneonnineaohdhabjbgopi\"]").remove();
  * @file contentScripts/content.js:L12-22
  */
 document.documentElement.style.opacity = 1;
-document.addEventListener("keydown", (e) => e.stopImmediatePropagation(), true);
-document.addEventListener("contextmenu", (e) => e.stopImmediatePropagation(), true);
+document.addEventListener("keydown", (e) => {
+  if ((e.key === "i" && e.ctrlKey && e.shiftKey) || e.key === "F12") {
+    e.stopImmediatePropagation();
+  }
+}, true);
+
+document.addEventListener("contextmenu", (e) => {
+  if (!excludeRewriteContextmenu.includes(location.hostname)) {
+    e.stopImmediatePropagation();
+  }
+}, true);
--- InterCLASS/remove_loading-screen.user.js
+++ InterCLASS/remove_loading-screen.user.js
@@ -11,6 +11,15 @@
 // @supportURL   https://blogbooks.net/shortlinks/discord/bbs-community
 // ==/UserScript==
 
+const excludeRewriteContextmenu = [
+  "discord.com",
+  "ptb.discord.com",
+  "canary.discord.com",
+  "vscode.dev",
+  "insider.vscode.dev",
+  "replit.com"
+]
+
 /**
  * @file contentScripts/content.js:L4-9, L108-128
  */
@@ -23,5 +32,14 @@ document.querySelector("[id^=\"jbddgjglgkkneonnineaohdhabjbgopi\"]").remove();
  * @file contentScripts/content.js:L12-22
  */
 document.documentElement.style.opacity = 1;
-document.addEventListener("keydown", (e) => e.stopImmediatePropagation(), true);
-document.addEventListener("contextmenu", (e) => e.stopImmediatePropagation(), true);
+document.addEventListener("keydown", (e) => {
+  if ((e.key === "i" && e.ctrlKey && e.shiftKey) || e.key === "F12") {
+    e.stopImmediatePropagation();
+  }
+}, true);
+
+document.addEventListener("contextmenu", (e) => {
+  if (!excludeRewriteContextmenu.includes(location.hostname)) {
+    e.stopImmediatePropagation();
+  }
+}, true);
